b85ae6ef33c184485b785bb7ef4b4d5b957d8936
Dariia <dariia.smyrnova@gmail.com>
2024-10-29 10:05:32 +0800

    typo

--- mdtest/main.go
+++ mdtest/main.go
@@ -411,7 +411,7 @@ func main() {
 
 	router := gin.Default()
     router.Use(cors.New(cors.Config{
-        AllowOrigins:     []string{"http://localhost:3000", "https://omnifunnel.ai/"}, 
+        AllowOrigins:     []string{"http://localhost:3000", "https://omnifunnel.ai"}, 
         AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "HEAD", "OPTIONS"},
         AllowHeaders:     []string{"Origin", "Content-Length", "Content-Type", "Authorization"},
         ExposeHeaders:    []string{"Content-Length"},
